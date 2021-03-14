e71c9c5095cbd0a2c77ded914967fa4f6241b6a4
cristoj <cristojvt@gmail.com>
2021-03-14 13:16:57 +0100

    docker configuration

--- src/js/general.js
+++ src/js/general.js
@@ -1,9 +1,13 @@
 const generalFn = function () {
-  const height = window.innerHeight;
-  const menuHeight = document.getElementById('headerNav').offsetHeight;
+    const height = window.innerHeight;
+    const $headerNav = document.getElementById('headerNav');
+    let menuHeight = 0;
+    if ($headerNav) {
+        menuHeight = $headerNav.offsetHeight;
+    }
 
-  document.querySelectorAll( '.mHeight100' ).forEach( function ( el ) {
-    el.style.minHeight = `${height - menuHeight}px`;
-  } );
+    document.querySelectorAll('.mHeight100').forEach(function (el) {
+        el.style.minHeight = `${height - menuHeight}px`;
+    });
 };
 export default generalFn;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,33 +1,34 @@
-const merge = require( "webpack-merge" );
-const path = require( "path" );
-const MiniCssExtractPlugin = require( "mini-css-extract-plugin" );
+const merge = require("webpack-merge");
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const common = require( "./webpack.common" );
+const common = require("./webpack.common");
 
-module.exports = merge( common, {
-  mode: "development",
+module.exports = merge(common, {
+    mode: "development",
+    devtool: "source-map",
+    output: {
+        publicPath: "/assets/",
+        filename: "[name].js",
+        chunkFilename: "[id].css"
+    },
 
-  output: {
-    publicPath: "/assets/",
-    filename     : "[name].js",
-    chunkFilename: "[id].css"
-  },
+    devServer: {
+        host: '172.22.0.2',
+        port: process.env.PORT || 3000,
+        contentBase: path.join(process.cwd(), "./site/public"),
+        watchContentBase: true,
+        quiet: false,
+        open: true,
+        historyApiFallback: {
+            rewrites: [{from: /./, to: "404.html"}]
+        }
+    },
 
-  devServer: {
-    port              : process.env.PORT || 3000,
-    contentBase       : path.join( process.cwd(), "./site/public" ),
-    watchContentBase  : true,
-    quiet             : false,
-    open              : true,
-    historyApiFallback: {
-      rewrites: [{ from: /./, to: "404.html" }]
-    }
-  },
-
-  plugins: [
-    new MiniCssExtractPlugin( {
-                                filename     : "[name].css",
-                                chunkFilename: "[id].css"
-                              } )
-  ]
-} );
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+});
